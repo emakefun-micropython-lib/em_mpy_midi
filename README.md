bb26860e3fc541cbc0701c4e44f6b94624db58b1
“xianlonluohan” <2047669296@qq.com>
2025-12-17 10:18:24 +0800

    修改程序，去除多余字样

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Emakefun SAM2695 MIDI
+# Emakefun MIDI
 
 ## Introduction
 
-SAM2695 MIDI module library. The SAM2695 MIDI module is a module used for synthesizing sound sources, with 128 universal MIDI sounds, a set of 128 variants, and dozens of percussion sounds. It can play ineffective 64 tone polyphonic music, as well as effective 38 tone music.
+MIDI module library. The MIDI module is a module used for synthesizing sound sources, with 128 universal MIDI sounds, a set of 128 variants, and dozens of percussion sounds. It can play ineffective 64 tone polyphonic music, as well as effective 38 tone music.
--- examples/play_music/play_music.py
+++ examples/play_music/play_music.py
@@ -1,24 +1,24 @@
 import time
 from micropython import const
 from machine import UART
-from sam2695_midi import Sam2695Midi
-from sam2695_midi_chorus_reverberation import SAM2695_MIDI_REVERBERATION_ROOM_2
-from sam2695_midi_note import (
-    SAM2695_MIDI_NOTE_G_4,
-    SAM2695_MIDI_NOTE_A_4,
-    SAM2695_MIDI_NOTE_C_5,
-    SAM2695_MIDI_NOTE_B_4,
-    SAM2695_MIDI_NOTE_D_5,
-    SAM2695_MIDI_NOTE_E_5,
-    SAM2695_MIDI_NOTE_G_5,
-    SAM2695_MIDI_NOTE_F_5,
+from midi import Midi
+from midi_chorus_reverberation import MIDI_REVERBERATION_ROOM_2
+from midi_note import (
+    MIDI_NOTE_G_4,
+    MIDI_NOTE_A_4,
+    MIDI_NOTE_C_5,
+    MIDI_NOTE_B_4,
+    MIDI_NOTE_D_5,
+    MIDI_NOTE_E_5,
+    MIDI_NOTE_G_5,
+    MIDI_NOTE_F_5,
 )
-from sam2695_midi_timbre import (
-    SAM2695_MIDI_TIMBRE_BANK_0,
-    SAM2695_MIDI_TIMBRE_BANK_0_ACOUSTIC_GUITAR_STEEL_STRING,
+from midi_timbre import (
+    MIDI_TIMBRE_BANK_0,
+    MIDI_TIMBRE_BANK_0_ACOUSTIC_GUITAR_STEEL_STRING,
 )
 
-SAM2695_MIDI_PIN: int = const(17)
+MIDI_PIN: int = const(17)
 
 CHANNEL: int = const(0)
 CHANNEL_VOLUME: int = const(110)
@@ -30,61 +30,61 @@ QUARTER_NOTE_DURATION: int = const(300)
 EIGHTH_NOTE_DURATION: int = 150
 DOTTED_QUARTER_NOTE_DURATION: int = const(450)
 
-uart = UART(2, baudrate=31250, tx=SAM2695_MIDI_PIN)
-sam2695_midi = Sam2695Midi(uart)
+uart = UART(2, baudrate=31250, tx=MIDI_PIN)
+midi = Midi(uart)
 
 
 def play_note(midi_note: int, duration: int, note_velocity: int = 90):
-    sam2695_midi.note_on(CHANNEL, midi_note, note_velocity)
+    midi.note_on(CHANNEL, midi_note, note_velocity)
     time.sleep_ms(duration)
-    sam2695_midi.note_off(CHANNEL, midi_note)
+    midi.note_off(CHANNEL, midi_note)
     time.sleep_ms(30)
 
 
-sam2695_midi.set_channel_timbre(
+midi.set_channel_timbre(
     CHANNEL,
-    SAM2695_MIDI_TIMBRE_BANK_0,
-    SAM2695_MIDI_TIMBRE_BANK_0_ACOUSTIC_GUITAR_STEEL_STRING,
+    MIDI_TIMBRE_BANK_0,
+    MIDI_TIMBRE_BANK_0_ACOUSTIC_GUITAR_STEEL_STRING,
 )
-sam2695_midi.set_channel_volume(CHANNEL, CHANNEL_VOLUME)
-sam2695_midi.set_reverberation(
+midi.set_channel_volume(CHANNEL, CHANNEL_VOLUME)
+midi.set_reverberation(
     CHANNEL,
-    SAM2695_MIDI_REVERBERATION_ROOM_2,
+    MIDI_REVERBERATION_ROOM_2,
     REVERBERATION_VOLUME,
     REVERBERATION_DELAY_FEEDBACK,
 )
 
 while True:
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_A_4, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_4, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_B_4, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_A_4, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_B_4, DOTTED_QUARTER_NOTE_DURATION)
     time.sleep_ms(QUARTER_NOTE_DURATION)
 
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_A_4, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_4, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_D_5, DOTTED_QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_A_4, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_D_5, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
     time.sleep_ms(QUARTER_NOTE_DURATION)
 
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_G_5, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_E_5, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_C_5, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_B_4, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_A_4, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_4, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_G_5, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_E_5, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_C_5, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_B_4, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_A_4, DOTTED_QUARTER_NOTE_DURATION)
     time.sleep_ms(QUARTER_NOTE_DURATION)
 
-    play_note(SAM2695_MIDI_NOTE_F_5, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_F_5, EIGHTH_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_E_5, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_C_5, QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_D_5, DOTTED_QUARTER_NOTE_DURATION)
-    play_note(SAM2695_MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_F_5, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_F_5, EIGHTH_NOTE_DURATION)
+    play_note(MIDI_NOTE_E_5, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_C_5, QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_D_5, DOTTED_QUARTER_NOTE_DURATION)
+    play_note(MIDI_NOTE_C_5, DOTTED_QUARTER_NOTE_DURATION)
 
     time.sleep_ms(5000)
